4c5747dc675c77710040c21546940ee7be8ad1a7
Dmitry <davydoff.dim2014@gmail.com>
2023-09-15 10:48:30 +0700

    second try

---
+++ OOP/Fraction.h
@@ -0,0 +1,17 @@
+#pragma once
+class Fraction
+{
+public:
+    Fraction();
+    Fraction(int numerator, int denominaor);//ñïèñîê èíèöèàëèçàöèè
+    ~Fraction() = default;
+    //Fraction sum(const Fraction a, const Fraction b);
+    Fraction sum(const Fraction b);
+    void print();
+    Fraction subtraction(const Fraction b);
+    Fraction multiplication(const Fraction b);
+    Fraction division(const Fraction b);
+    int numerator;
+    int denominator;
+};
+

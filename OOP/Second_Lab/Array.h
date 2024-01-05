54792e3ccd0f7f6cbc87969c2cab58e971e1c6d8
Dmitry <davydoff.dim2014@gmail.com>
2024-01-05 15:59:06 +1000

    Добавлено: конструктор копирования, size, константный оператор []

--- OOP/Second_Lab/Array.h
+++ OOP/Second_Lab/Array.h
@@ -1,17 +1,19 @@
 #pragma once
-
 class Array
 {
 public:
     Array(const int size = 10, const int value = 0);
     ~Array();
-
     void print() const;
-
-    int operator[](const int index);
+    int &operator[](const int index);
+    //
+    Array(const Array &other);
+    int size() const;
+    const int &operator[](const int index) const;
+    //
 
 private:
-    int *m_array = nullptr;
+    int* m_array = nullptr;
     int m_size = 0;
 };
 
--- OOP/Second_Lab/main.cpp
+++ OOP/Second_Lab/main.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
 #include "Array.h"
 
-int main() {
+double calculateAvg(Array arr)
+{
+    double avg = 0;
+    if (arr.size() == 0) return 0;
+
+    for (int i = 0; i < arr.size(); i++)
+        avg += arr[i];
+    avg /= arr.size();
+    return avg;
+}
+int main()
+{
+    using std::cout;
+    using std::cin;
+    using std::endl;
+
     Array arr(10, 0);
-    std::cout << arr[3] << ' ' << arr[9] << std::endl;
+    cout << arr[3] << ' ' << arr[9] << endl;
+    arr[3] = 4;
+    arr[5] = arr[3];
+    cout << calculateAvg(arr) << endl;
     arr.print();
-    getchar();
     return 0;
 }

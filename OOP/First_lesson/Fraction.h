a610ea15f2dab9d6c5d0e7dc8c61b82434a54dc5
Dmitry <davydoff.dim2014@gmail.com>
2024-01-05 14:20:02 +1000

    добавлен конструктор (по умолчанию + как минимум один конструктор с параметрами);

--- OOP/First_lesson/Fraction.h
+++ OOP/First_lesson/Fraction.h
@@ -8,34 +8,6 @@ public:
     Fraction(const int numerator, const int denominator);
     ~Fraction() = default;
 
-    int getNumerator() const;
-    int getDenominator() const;
-
-    void setNumerator(const int numerator);
-    void setDenominator(const int denominator);
-
-    void print() const;
-    void input();
-    Fraction summation(const Fraction other) const;
-    Fraction subtraction(const Fraction other) const;
-    Fraction multiplication(const Fraction other) const;
-    Fraction division(const Fraction other) const;
-    void reduce();
-
-    bool operator==(const Fraction other) const;
-    bool operator!=(const Fraction other) const;
-    bool operator<(const Fraction other) const;
-    bool operator>(const Fraction other) const;
-    Fraction operator+(const Fraction other) const;
-    Fraction operator-(const Fraction other) const;
-    Fraction operator*(const Fraction other) const;
-    Fraction operator/(const Fraction other) const;
-
-    // Custom methods
-    void intToFraction(const int integer);
-    void multiplyByInt(const int integer);
-    void exponentiation(const int degree);
-
 private:
     int m_numerator = 0;
     int m_denominator = 1;

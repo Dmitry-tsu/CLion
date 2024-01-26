aab7cff1e0cc18d21c267980430efbcf9be8a6f6
Dmitry <davydoff.dim2014@gmail.com>
2024-01-27 00:19:22 +0700

    Добавлено: функция printDistanceMatrix + пример вывода, printCurrentPath, isPathComplete, printShortestPath

--- ASD/branch_and_bound method/branch_and_bound method.cpp
+++ ASD/branch_and_bound method/branch_and_bound method.cpp
@@ -3,6 +3,7 @@
 #include <random>
 #include <limits>
 #include <set>
+#include <utility>
 #include <vector>
 
 typedef size_t TownIndex;
@@ -11,8 +12,9 @@ typedef int Distance;
 typedef std::vector<Distance> DistanceRow;
 typedef std::vector<DistanceRow> DistanceMatrix;
 typedef std::set<TownIndex> TownSet;
+
 class TravelingSalesman
- {
+{
     DistanceMatrix distanceMatrix;
     PathValue currentPath;
     Distance currentPathLength;
@@ -20,11 +22,122 @@ class TravelingSalesman
     Distance shortestPathLength;
     TownSet unvisitedTowns;
 
-    public:
+public:
+    void printDistanceMatrix()
+    {
+        std::cout << "\nDistance Matrix:\n";
+        for (const auto& row : distanceMatrix)
+        {
+            for (const auto& distance : row) {
+                std::cout << distance << "\t";
+            }
+            std::cout << "\n";
+        }
+    }
 
- };
+    TravelingSalesman(DistanceMatrix distances)
+            : distanceMatrix(std::move(distances)), shortestPathLength(std::numeric_limits<Distance>::max())
+    {
+
+    }
+
+    void findAndPrintShortestPath()
+    {
+
+    }
+
+private:
+    void printCurrentPath()
+    {
+        std::cout << "Current Path: ";
+        for (auto townIndex : currentPath)
+        {
+            std::cout << townIndex + 1 << '\t';
+        }
+        std::cout << "Length: " << currentPathLength << std::endl;
+    }
+
+    void addTownWithDeltaDistance(TownIndex town, Distance deltaDistance = 0)
+    {
+
+    }
+
+    void findShortestPath()
+    {
+
+    }
+
+    bool tryToImproveCurrentPath()
+    {
+
+    }
+
+    bool isPathComplete()
+    {
+        return currentPath.size() >= distanceMatrix.size() + 1;
+    }
+
+    bool tryToAddNextOptimalTown()
+    {
+
+    }
+
+    bool tryToAddOptimalTown(TownIndex town)
+    {
+
+    }
+
+    bool tryToAddNextOptimalUnvisitedTownNotLessThan(TownIndex townStart)
+    {
+
+    }
+
+    bool tryBackingUpTown()
+    {
+
+    }
+
+    TownIndex popAndGetCurrentTown()
+    {
+
+    }
+
+    void printShortestPath()
+    {
+        std::cout << std::endl;
+        for (auto townIndex : shortestPath)
+        {
+            std::cout << townIndex + 1 << '\t';
+        }
+        std::cout << std::endl;
+    }
+
+};
 
 int main()
 {
+    std::ios::sync_with_stdio(false);
+    int totalTowns{};
 
+    do  {
+        std::cout << "Total towns (>= 2): ";
+        std::cin >> totalTowns;
+    } while (totalTowns < 2);
+
+    std::cout << "Enter distances between towns:" << std::endl;
+    DistanceMatrix distances(totalTowns, DistanceRow(totalTowns));
+    for (TownIndex from{}; from < TownIndex(totalTowns); ++from)
+    {
+        for (TownIndex to{}; to < TownIndex(totalTowns); ++to)
+        {
+            if (from == to)
+            {
+                continue;
+            }
+            std::cout << from + 1 << " - " << to + 1 << "\t: ";
+            std::cin >> distances[from][to];
+        }
+    }
+    TravelingSalesman salesman(distances);
+    salesman.printDistanceMatrix();
 }

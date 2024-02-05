494f25ba158053b0446662c42adeb52f23b8f6ad
Dmitry <davydoff.dim2014@gmail.com>
2024-02-05 15:10:14 +0700

    создание проекта

---
+++ OOP/Sixth_Lab/Set.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "../../CLion/OOP/Third_Lab/BoolVector.h"
+#include "../../CLion/OOP/Fourth_Lab/BoolMatrix.h"
+
+class Set:  public BoolVector
+{
+public:
+
+
+private:
+
+};
---
+++ OOP/Sixth_Lab/main.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include "Set.h"
+
+int main()
+{
+
+    return 0;
+}
